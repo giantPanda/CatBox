b4225edb259a0df63b8d7fd175111830ffd49740
cwindeler <claus.windeler@billsafe.de>
2015-04-24 16:56:59 +0200

    Added screenshot function

--- app/src/main/java/io/box/catbox/NewCatActivity.java
+++ app/src/main/java/io/box/catbox/NewCatActivity.java
@@ -1,28 +1,41 @@
 package io.box.catbox;
 
 import android.app.DatePickerDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 
 public class NewCatActivity extends ActionBarActivity implements View.OnClickListener {
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
     private EditText newCatBirthdate;
+    private ImageView newCatImage;
     private DatePickerDialog newCatBirthdateDatePickerDialog;
     private SimpleDateFormat dateFormat;
+    private Intent imageCaptureIntent;
+    private Uri catImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,14 @@ public class NewCatActivity extends ActionBarActivity implements View.OnClickLis
         newCatBirthdate = (EditText) findViewById(R.id.new_cat_birthdate);
         newCatBirthdate.setInputType(InputType.TYPE_NULL);
         newCatBirthdate.setOnClickListener(this);
+
+        newCatImage = (ImageView) findViewById(R.id.new_cat_image);
+        newCatImage.setOnClickListener(this);
+
+        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        catImageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        imageCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, catImageUri);
 
         Calendar newCalendar = Calendar.getInstance();
         newCatBirthdateDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
@@ -81,6 +102,55 @@ public class NewCatActivity extends ActionBarActivity implements View.OnClickLis
     public void onClick(View v) {
         if (v == newCatBirthdate) {
             newCatBirthdateDatePickerDialog.show();
+        } else if (v == newCatImage) {
+            startActivityForResult(imageCaptureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CatBox");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("CatBox", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                newCatImage.setImageURI(catImageUri);
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
         }
     }
 }
